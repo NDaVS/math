994d50f38ccb1ac2d37e3bfbb6579709a24acee0
UserNameMaybe <fandjbro@gmail.com>
2024-04-08 22:18:59 +1000

    third lab calculus math made by Lunk, feat IDrumo

--- calculus/labs/4_lab.py
+++ calculus/labs/4_lab.py
@@ -4,21 +4,27 @@ from calculus.labs.support.checker import check_results
 import sympy as sp
 import math
 
+# made by Lunk, feat IDrumo
 
 def main():
-    x = sp.symbols('x')
-    f = x / 2 - sp.cos(2/x)
+    k, m, n, = 2, 4, 4  # parameters
     a, b = 0.4, 0.9
-    n = 10
-    h = round((b - a) / 10, 2)
-    all_x = [a + i * h for i in range(4)]
-    all_y = [f.subs(x, xi) for xi in all_x]
-    new_f = lagrange(all_x, all_y, x)
-    new_df = dnf(new_f, x, 2)
-    df = dnf(f, x, 2)
-    print(new_df.subs(x, a+4*h))
-    print(df.subs(x, a+3*h))
-    check_results(df, new_df, x, a, all_x[-1], 3, a+4*h)
+    n_dots = 10
+
+    x = sp.symbols('x')  # original function
+    f = x / 2 - sp.cos(2 / x)
+
+    h = round((b - a) / n_dots, 2)  # step
+
+    all_x = [a + i * h for i in range(n)]  # array of xs
+    all_y = [f.subs(x, xi) for xi in all_x]  # array of ys
+
+    new_f = lagrange(all_x, all_y, x)  # lagrange function
+
+    new_df = dnf(new_f, x, k)  # original function differentiate
+    df = dnf(f, x, k)  # lagrange function differentiate
+
+    check_results(df, new_df, x, all_x[0], all_x[-1], n, a + m * h)  # result
 
 
 if __name__ == '__main__':
