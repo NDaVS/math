a28c3b480973aa226d5095267e441af07630d72f
vershinin.da <fandjbro@gmail.com>
2024-10-15 16:09:29 +1000

    6 lab

--- calculus/labs/main.py
+++ calculus/labs/main.py
@@ -1,28 +1,81 @@
-import math
+import numpy as np
+
+from calcus_math.pract.support.monotonous_running import tridiagonal_solver, checker
 
 
-def do_sin(x, n):
-    return (-1) ** (n - 1) * x ** (2 * n - 1) / math.factorial(2 * n - 1)
+def TDMA(a, b, c, f):
+    a, b, c, f = tuple(map(lambda k_list: list(map(float, k_list)), (a, b, c, f)))
+
+    alpha = [-b[0] / c[0]]
+    beta = [f[0] / c[0]]
+    n = len(f)
+    x = [0] * n
+
+    for i in range(1, n):
+        alpha.append(-b[i] / (a[i] * alpha[i - 1] + c[i]))
+        beta.append((f[i] - a[i] * beta[i - 1]) / (a[i] * alpha[i - 1] + c[i]))
+
+    x[n - 1] = beta[n - 1]
+
+    for i in range(n - 1, -1, -1):
+        x[i - 1] = alpha[i - 1] * x[i] + beta[i - 1]
+
+    return x
 
 
-def sin(degree, accuracy):
-    answer = 0
-    n = 1
-    x = degree
-    if x >= 2 * math.pi:
-        x %= 2 * math.pi
-    while abs(do_sin(x, n)) >= accuracy:
-        answer += do_sin(x, n)
-        n += 1
-    print('=' * 60)
-    print(f"Приближённое значение для sin({degree})≈{answer}\n"
-          f"Точность вычислений: {accuracy}\n"
-          f"Точное значение для sin({degree})= {math.sin(x)}\n"
-          f"Погрешность: {abs(answer - math.sin(x))}\n"
-          f"Погрешность{'не' if abs(answer - math.sin(x)) > accuracy else ''} удовлетворяет выбранной точности")
+def main():
+    # A = np.array([
+    #     [2, -1, 0],
+    #     [-1, 2, -1],
+    #     [0, -1, 2]
+    # ])
+    # b = np.array([1, 2, 3])
+
+    # A = np.array([
+    #     [3, 6, 0],
+    #     [1, 12, 2],
+    #     [0, 3, 2]
+    # ])
+    # b = np.array([4, 7, 12])
+
+    # A = np.array([
+    #     [1, 2, 0],
+    #     [2, 1, 3],
+    #     [4, 1, 2],
+    #     [0, 2, 1]
+    # ])
+    # b = np.array([4, 2, 3, 5])
+
+    A = np.array([
+    [4.0, 4.0, 0.0, 0.0, 0.0, 0.0],
+    [7.0, 5.0, -2.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 10.0, -10.0, 0.0, 0.0],
+    [0.0, 0.0, -5.0, 4.0, 9.0, 0.0],
+    [0.0, 0.0, 0.0, -2.0, 6.0, -4.0],
+    [0.0, 0.0, 0.0, -1.0, -7.0, 1.0],
+
+])
+    b = np.array([9, 3, 1, 2, 7, -1.6])
+
+    x = tridiagonal_solver(A, b)
+    print(sum(x))
+    print(checker(A, b, x))
 
 
-if __name__ == '__main__':
-    sin(0.5236, 10 ** (-4))
-    sin(52.36, 10 ** (-8))
-    print('=' * 60)
+def dev():
+    # Входные данные
+    a = [1.0, 2.0, 3.0, 4.0]
+    b = [-2.0, -3.0, -4.0, -5.0]
+    c = [1.0, 2.0, 3.0, 4.0]
+    f = [1.0, 2.0, 3.0, 4.0]
+
+    # Вызов функции TDMA
+    x = TDMA(a, b, c, f)
+
+    # Вывод результата
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
+    # dev()
