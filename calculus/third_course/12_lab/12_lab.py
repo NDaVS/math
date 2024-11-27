2a75f8060d13b805deb9c29ef2eff6531ae32e04
fandj <fandjbro@gmail.com>
2024-11-27 19:58:29 +1000

    простая итерация для СЗ

---
+++ calculus/third_course/12_lab/12_lab.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+class SimpleIteration:
+    def __init__(self, A: np.array, tol: float = 1e-5):
+        self._tol = tol
+        self._A = A
+
+    def _compute(self, x: np.array):
+        y = self._A @ x
+        lmd = y @ x
+        x_new = y / np.linalg.norm(y)
+        return lmd, x_new
+
+    def compute(self):
+        x = np.ones(self._A.shape[0])
+        lmd, x_old = self._compute(x)
+
+        while True:
+            lmd_new, x_new = self._compute(x_old)
+
+            if np.linalg.norm(x_old - x_new) < self._tol:
+                return lmd_new, x_new
+
+            lmd = lmd_new
+            x_old = x_new
+
+
+def main():
+    tol = 1e-5
+    A = np.array([[1.00, 0.42, 0.54, 0.66],
+                  [0.42, 1.00, 0.32, 0.44],
+                  [0.54, 0.32, 1.00, 0.22],
+                  [0.66, 0.44, 0.22, 1.00]])
+
+    iterator = SimpleIteration(A, tol)
+    lmd, s_vector = iterator.compute()
+    print("Наибольшее собственное значение = ", lmd)
+    print("Собственный вектор соответствующий этому значению: ", s_vector)
+    print("Проверка вида Ax - lmd * x = ", np.linalg.norm(A @ s_vector - lmd * s_vector))
+
+
+if __name__ == '__main__':
+    main()
